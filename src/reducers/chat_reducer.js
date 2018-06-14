5d7ab1aa40f32f149a426945aa525f4abe9d5615
Scott Bowler <scott.bowler@learningfuze.com>
2018-06-13 17:27:33 -0700

    Finished multi room functionality

--- src/reducers/chat_reducer.js
+++ src/reducers/chat_reducer.js
@@ -2,13 +2,20 @@ import types from '../actions/types';
 
 const DEFAULT_STATE = {
     log: {},
-    name: ''
+    name: '',
+    rooms: {}
 };
 
 export default (state = DEFAULT_STATE, action) => {
     switch(action.type){
+        case types.SET_ROOM:
+            return {...state, name: action.payload};
+        case types.UPDATE_ROOMS:
+            return {...state, rooms: action.payload};
         case types.UPDATE_CHAT_LOG:
-            return {...state, log: action.chatLog, name: action.name};
+            return {...state, log: action.chatLog};
+        case types.CLEAR_CHAT_DATA:
+            return {log: {}, name: '', rooms: {}}
         default:
             return state;
     }
