62db2b0ede2348aae1fee23dc1daec2d31999824
samskarvan <samskarvan@gmail.com>
2018-06-15 15:58:41 -0700

    enabled email sign up within firebase  and created new user

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
+import React, {Component} from 'react';
 import { Route } from 'react-router-dom';
+import {auth} from '../firebase';
 import Nav from './nav';
 import Home from './home';
 import Chat from './chat';
@@ -8,17 +9,28 @@ import CreateChatRoom from './create_chat_room';
 import ChatRooms from './chat_rooms';
 import SignUp from './sign_up';
 
-const App = () => (
-    <div>
-        <Nav/>
-        <div className="container">
-            <Route exact path="/" component={Home}/>
-            <Route path="/sign-up" component={SignUp}/>
-            <Route path="/chat/:id" component={Chat}/>
-            <Route path="/chat-rooms" component={ChatRooms}/>
-            <Route path="/create-room" component={CreateChatRoom}/>
-        </div>
-    </div>
-);
+class App extends Component{
+    componentDidMount(){
+        auth.onAuthStateChanged(user=>{
+            user ? console.log('user:', user.displayName) : console.log('no user found');
+        })
+    }
+    render(){
+        return(
+            <div>
+                <Nav/>
+                <div className="container">
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/sign-up" component={SignUp}/>
+                    <Route path="/chat/:id" component={Chat}/>
+                    <Route path="/chat-rooms" component={ChatRooms}/>
+                    <Route path="/create-room" component={CreateChatRoom}/>
+                </div>
+            </div>
+        )
+    }
+
+}
+   
 
 export default App;
